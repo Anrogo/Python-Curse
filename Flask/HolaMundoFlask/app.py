0c93af768857d125d4883cced255bafe3d5b90ff
anrogo <antoniorogo96@gmail.com>
2022-08-07 13:16:45 +0200

    flask y json

--- Flask/HolaMundoFlask/app.py
+++ Flask/HolaMundoFlask/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, url_for, jsonify
+from werkzeug.exceptions import abort
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def inicio():
     # app.logger.debug('Mensaje de debug')
     app.logger.info(f'Entramos al path: {request.path}')
     return "<h1>Hola mundo!</h1>"
@@ -16,3 +18,27 @@ def saludar(nombre):
 @app.route('/edad/<int:edad>')
 def edad(edad):
     return f'Tienes {edad} años'
+
+@app.route('/mostrar/<nombre>', methods=['GET', 'POST'])
+def mostrar_nombre(nombre):
+    return render_template('mostrar.html', nombre_llave=nombre)
+
+@app.route('/redireccionar')
+def redireccionar():
+    return redirect(url_for('mostrar_nombre', nombre='Juan'))
+
+@app.route('/salir')
+def salir():
+    return abort(404)
+
+# Captura los errores que se produzcan y muestra la plantilla personalizada:
+@app.errorhandler(404)
+def pagina_no_encontrada(error):
+    return render_template('error404.html', error=error), 404
+
+
+# REST Representational State Transfer
+@app.route('/api/mostrar/<nombre>', methods=['GET', 'POST'])
+def mostrar_json(nombre):
+    valores = {'nombre': nombre, 'metodo_http': request.method}
+    return jsonify(valores)
